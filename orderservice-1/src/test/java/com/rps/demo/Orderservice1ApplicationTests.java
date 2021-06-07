9e0c14a5aee7afd105668e644254ec9733663c56
SANDEEP <SANDEEP@DESKTOP-7G2JT9N>
2021-06-07 16:43:19 +0530

    issue fixed

---
+++ orderservice-1/src/test/java/com/rps/demo/Orderservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rps.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Orderservice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
